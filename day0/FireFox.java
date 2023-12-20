62ac2c05fd6dc560bf61e3aa8e9273fbf12cad48
Suganthi-QA <154419969+Suganthi-QA@users.noreply.github.com>
2023-12-20 15:13:36 +0530

    Add files via upload
    Day1

---
+++ day0/FireFox.java
@@ -0,0 +1,23 @@
+package Week1.day0;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float browserVersion = 100.2f; //(use dataType float)
+		String browserName = "firefox" ;//(use dataType String)
+		 Boolean isVisible = true ;//(use dataType boolean)
+		int releaseYear = 1998 ;//(use dataType int)
+	     char browserLogo = 'f'; //(use datatype char)
+	     System.out.println("Firefox Browser Information");
+	     System.out.println("****************************");
+	     System.out.println("Browser Version:" +browserVersion);
+	     System.out.println("Browser Name:" +browserName);
+	     System.out.println("Browser is visible?"+isVisible);
+	     System.out.println("Browser Released Year:"+releaseYear);
+	     System.out.println("Browser Logo:"+browserLogo);
+	     
+
+	}
+
+}

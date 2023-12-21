2a29f0d4a4b3c780a880c323650c1ca74a14f754
Suganthi-QA <154419969+Suganthi-QA@users.noreply.github.com>
2023-12-21 12:04:33 +0530

    Add files via upload

---
+++ day2/Browser.java
@@ -0,0 +1,31 @@
+package Week1.day2;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Browser Name=new Browser();
+		
+		String Browsername=Name.launchBrowser("Google Chrome");
+		System.out.println(Browsername);
+		
+		System.out.println("*******************************");
+		
+
+	}
+	
+	//methods
+	public String launchBrowser(String browserName)
+	
+	{
+		System.out.println("Browser Launched Successfully");
+		return browserName;
+	}
+	
+	public void loadUrl()
+	{
+		System.out.println("Application url loaded successfully");
+		
+	}
+
+}
